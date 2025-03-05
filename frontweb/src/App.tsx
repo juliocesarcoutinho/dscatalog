d5a990de53798945d7062db03516a0d9d4456403
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2025-03-04 23:36:17 -0300

    continuity

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -2,10 +2,12 @@ import './assets/styles/custom.scss'
 import './App.css';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
 
 const App = () => {
     return (
-        <Home/>
+        // <Home/>
+        <Catalog/>
     );
 }
 
